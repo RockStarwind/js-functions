afbfbb33c359b2ed3acda99075492b1efca1d879
seto89 <st.to.1989@gmail.com>
2019-11-10 14:22:17 -0500

    deepClone can check for sets now

--- deepclone.js
+++ deepclone.js
@@ -1,11 +1,13 @@
 /*
-# Deep Cloning Objects and Arrays
-A Javascript function that deep clones objects and arrays
-Possibly faster than JSON.parse(JSON.stringify(obj)).
-
-Author: seto89
-Repo: https://github.com/seto89/js-functions
-License: MIT
+* # Deep Cloning Objects and Arrays
+* A Javascript function that deep clones objects and arrays
+* Possibly faster than JSON.parse(JSON.stringify(obj)).
+*
+* Author: seto89
+* Date: 2019-11-10
+* Version: 1.0.1
+* Repo: https://github.com/seto89/js-functions
+* License: MIT
 
 Polyfill for Object.assign:
   - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/assign#Polyfill
@@ -18,6 +20,9 @@ function deepClone(obj) {
   function isHash(obj) {
     return Object.prototype.toString.call(obj) === "[object Object]";
   }
+  function isSet(obj) {
+    return Object.prototype.toString.call(obj) === "[object Set]"
+  }
   function arrayShallowClone(obj) {
     return obj.slice(0);
   }
@@ -26,6 +31,15 @@ function deepClone(obj) {
   }
 
   var newObj;
+  var set = false;
+  // Is this a set?
+  if (isSet(obj)) {
+    // Convert the set into an array
+    newObj = [];
+    obj.forEach(function (value) { return newObj.push(value) });
+    obj = arrayShallowClone(newObj);
+    set = true;
+  }
   // Is this an array or a hash table?
   if (isArray(obj) || isHash(obj)) {
     // Is this an array?
@@ -45,6 +59,11 @@ function deepClone(obj) {
       for (var prop in newObj) {
         newObj[prop] = deepClone(newObj[prop]);
       }
+    }
+    // Did this object begin as a set?
+    if (set) {
+      // Convert it back
+      newObj = new Set(newObj.slice(0));
     }
     return newObj;
   }
