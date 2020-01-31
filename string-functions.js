8877df5f696d7e04ba6275ba034643f2d1317064
Rock Starwind <rockstarwinddev@gmail.com>
2020-01-31 09:15:37 -0500

    renamed string.toAltCase.js to string-functions.js, added isLowerCase() and isUpperCase() functions

--- string-functions.js
+++ string-functions.js
@@ -5,6 +5,44 @@
  Repo: https://github.com/RockStarwind/js-functions
  License: MIT
  ─────────────────────────────────────────────────────────────────────────────
+*/
+
+/*
+ # isLowerCase()
+ How to use:
+   "string".isLowerCase();
+   If result is true: All alphabetical characters in the string are in lowercase.
+   If result is false: All alphabetical characters in the string aren't in lowercase.
+   If result is null: There are no alphabetical characters in the string.
+*/
+String.prototype.isLowerCase = function() {
+	'use strict';
+	if ((/([a-zA-Z\u00BF-\u1FFF\u2C00-\uD7FF])/i).test(this)) {
+		return this === this.toLowerCase()
+	} else {
+		return null
+	}
+}
+
+/*
+ # isUpperCase()
+ How to use:
+   "string".isUpperCase();
+   If result is true: All alphabetical characters in the string are in uppercase.
+   If result is false: All alphabetical characters in the string aren't in uppercase.
+   If result is null: There are no alphabetical characters in the string.
+*/
+String.prototype.isUpperCase = function() {
+	'use strict';
+	if ((/([a-zA-Z\u00BF-\u1FFF\u2C00-\uD7FF])/i).test(this)) {
+		return this === this.toUpperCase()
+	} else {
+		return null
+	}
+}
+
+/*
+ # toAltCase()
  How to use:
    "string".toAltCase(lowercase);
    * lowercase: false by default, set to true to start altcasing in lowercase
