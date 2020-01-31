07e25dd0c0d1abda8136ec175360a00a6f4b5885
Rock Starwind <rockstarwinddev@gmail.com>
2020-01-31 08:36:17 -0500

    First Commit + New function

---
+++ string.toAltCase.js
@@ -0,0 +1,28 @@
+// toAltCase by @RockStarwind
+// MIT License
+// How to use:
+//	"string".toAltCase(lowercase);
+//	* lowercase: false by default, set to true to start altcasing in lowercase
+/*
+ ─────────────────────────────────────────────────────────────────────────────
+ Author: @RockStarwind
+ Date: 2020-01-31
+ Repo: https://github.com/RockStarwind/js-functions
+ License: MIT
+ ─────────────────────────────────────────────────────────────────────────────
+ How to use:
+   "string".toAltCase(lowercase);
+   * lowercase: false by default, set to true to start altcasing in lowercase
+*/
+String.prototype.toAltCase = function(lowercase) {
+	'use strict';
+	lowercase = (lowercase === true);
+	var str = this.split("");
+	for (var i = 0; i < str.length; i++) {
+		if ((/([A-Za-z\u00C0-\u1FFF\u2800-\uFFFD])/i).test(str[i])) {
+			str[i] = (lowercase) ? str[i].toLowerCase() : str[i].toUpperCase()
+			lowercase = !lowercase;
+		}
+	}
+	return str.join("");
+}
